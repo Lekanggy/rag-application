726ed28aed4d7f0cbb3e9e49e977350f930c0781
lekanggy <lekanggy12@gmail.com>
2026-07-05 14:33:46 +0100

    evaluation added

---
+++ evaluation_helper.py
@@ -0,0 +1,25 @@
+from ingest import load_faq_data
+from pydantic import BaseModel
+
+
+#Build the insruction of  LLM for document generation
+data_gen_instructions = """
+You emulate a student who's taking our course.
+Formulate 5 questions this student might ask based on a FAQ record. The record
+should contain the answer to the questions, and the questions should be complete and not too short.
+If possible, use as fewer words as possible from the record.
+
+The output should resemble how people ask questions
+on the internet. Not too formal, not too short, not too long.
+""".strip()
+
+
+def get_documents(filer="llm-zoomcamp"):
+    documents = load_faq_data()
+    documents_llm = [doc for doc in documents if doc["course"] == filer]
+    return documents_llm
+
+
+
+class Questions(BaseModel):
+    questions: list[str]
