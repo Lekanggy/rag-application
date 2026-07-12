f68f0a19b3dcad76dd1f8d4af726a16c1ae070c1
lekanggy <lekanggy12@gmail.com>
2026-07-12 06:49:53 +0100

    update LLM-as a judge

--- client.py
+++ client.py
@@ -8,3 +8,10 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY"),
 )
+
+client2 = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = os.getenv("NVIDIA_API_KEY"),
+)
+
+
